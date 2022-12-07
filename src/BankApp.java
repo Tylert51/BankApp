c8ea185418c48f0e5d917848a5948ba39078a020
tyler <ttang7910@bths.edu>
2022-12-06 19:07:25 -0500

    finished BankApp.java

--- src/BankApp.java
+++ src/BankApp.java
@@ -4,11 +4,12 @@ public class BankApp {
     private Bank bank;
     private BagelShop bagelShop;
     private CreditCard creditCard;
-    private boolean twoCreditCards;
+    private CreditCard creditCard2;
+    private boolean hasTwoCreditCards;
 
     public BankApp(String name, String personalPIN) {
         this.name = name;
-        twoCreditCards = false;
+        hasTwoCreditCards = false;
 
         int randInv = (int) (Math.random() * 1001 + 1000);
 
@@ -25,7 +26,83 @@ public class BankApp {
                 "\n(4) Compare credit card balances" +
                 "\n(5) Deposit profits into the bank" +
                 "\n(6) Check inventory" +
-                "\n(7) Quit";
+                "\n(7) Check banking account" +
+                "\n(8) Check credit card balance" +
+                "\n(9) Quit";
+    }
 
+    public String purchaseBagel(CreditCard credit, int quantity, String PIN) {
+        if(bagelShop.payForBagels(credit, quantity, PIN)) {
+            return "Payment successful!" +
+                    "\nBalanced Owed: " + credit.getBalanceOwed() +
+                    "\n" + bagelShop;
+        } else {
+            return "Purchase not successful";
+        }
+    }
+
+    public String returnBagel(CreditCard credit, int quantity, String PIN) {
+        if(bagelShop.returnBagels(credit, quantity, PIN)) {
+            return "Return Successful!" +
+                    "\nBalanced Owed: " + credit.getBalanceOwed() +
+                    "\n" + bagelShop;
+        } else {
+            return "Return not successful";
+        }
+    }
+
+    public String secondCreditCard(String name, String PIN) {
+        if (!hasTwoCreditCards) {
+            hasTwoCreditCards = true;
+            creditCard2 = new CreditCard(name, PIN);
+            return "You have successfully created a new credit card account";
+        } else {
+            return "You already have two credit card accounts";
+        }
+    }
+
+    public String compareCreditCardBal() {
+        if (hasTwoCreditCards) {
+            return "This is the credit card account with the lower balance owed: " +
+                    "\n" + bank.lowerBalance(creditCard, creditCard2);
+        } else {
+            return "You only have one card";
+        }
+    }
+
+    public String depositProfits() {
+        bagelShop.depositProfits();
+        return "Successful deposit of profits into bank";
+    }
+
+    public String checkOnShop() {
+        return bagelShop.toString();
+    }
+
+    public String checkOnBank() {
+        return bank.toString();
+    }
+
+    public boolean getTwoCredit() {
+        return hasTwoCreditCards;
+    }
+
+    public CreditCard getCreditCard() {
+        return creditCard;
+    }
+
+    public CreditCard getCreditCard2() {
+        return creditCard2;
+    }
+
+    public String showCreditCardInfo() {
+        if(hasTwoCreditCards) {
+            return "Credit Card 1: " +
+                    "\n" + creditCard +
+                    "\n\nCredit Card 2: " +
+                    "\n" + creditCard2;
+        } else {
+            return "Credit Card Info: " + creditCard;
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,87 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
 
+        System.out.print("Hi! Welcome to my bank app where you can buy, sell, and manage your own bagel shop!\nWhat is your name: ");
+        String name = s.nextLine();
+        System.out.print("What would you like your credit card pin to be: ");
+        String PIN = s.nextLine();
+
+        BankApp app = new BankApp(name, PIN);
+        CreditCard mainCredit = app.getCreditCard();
+        CreditCard secondaryCard = null;
+
+        System.out.println("Here is the menu of available actions: \n");
+        System.out.print(app.printMenu() + "\nEnter your action: ");
+        String answer = s.nextLine();
+
+        CreditCard paymentCard = mainCredit;
+
+        while(isRunning(answer)) {
+            if (answer.equals("3")) {
+                System.out.print("What would you like for your pin to be: ");
+                PIN = s.nextLine();
+                System.out.println(app.secondCreditCard(name, PIN));
+
+                secondaryCard = app.getCreditCard2();
+
+            } else if (answer.equals("1")) {
+                System.out.print("How many bagels would you like to purchase: ");
+                int quant = Integer.parseInt(s.nextLine());
+                if (app.getTwoCredit()) {
+                    System.out.print("What credit card would you like to use (1) or (2): ");
+                    answer = s.nextLine();
+                    if (answer.equals("2")) {
+                        paymentCard = secondaryCard;
+                    } else {
+                        paymentCard = mainCredit;
+                    }
+                }
+                System.out.print("What is your pin: ");
+                PIN = s.nextLine();
+                System.out.println(app.purchaseBagel(paymentCard, quant, PIN));
+
+            } else if (answer.equals("2")) {
+                System.out.print("How many bagels would you like to return: ");
+                int quant = Integer.parseInt(s.nextLine());
+                if (app.getTwoCredit()) {
+                    System.out.print("What credit card would you like to use (1) or (2): ");
+                    answer = s.nextLine();
+                    if (answer.equals("2")) {
+                        paymentCard = secondaryCard;
+                    }
+                }
+                System.out.print("What is your pin: ");
+                PIN = s.nextLine();
+                System.out.println(app.returnBagel(paymentCard, quant, PIN));
+
+            } else if (answer.equals("4")) {
+                System.out.println(app.compareCreditCardBal());
+
+            } else if (answer.equals("5")) {
+                System.out.println(app.depositProfits());
+
+            } else if (answer.equals("6")) {
+                System.out.println(app.checkOnShop());
+
+            } else if (answer.equals("7")) {
+                System.out.println(app.checkOnBank());
+
+            } else if (answer.equals("8")) {
+                System.out.println(app.showCreditCardInfo());
+
+            }
+
+            System.out.print("\n\n\n" + app.printMenu() + "\nEnter your action: ");
+            answer = s.nextLine();
+        }
+        System.out.println("Successfully quit application");
+
+    }
+    public static boolean isRunning(String ans) {
+        String possibleAns = "12345678";
+        return possibleAns.contains(ans);
     }
 }
